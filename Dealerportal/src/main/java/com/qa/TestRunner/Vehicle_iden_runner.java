aa14479c687fab5b3ed3fe0ae9b6988dbd61a4ed
sakunthalak <39465777+sakunthalak@users.noreply.github.com>
2019-06-08 07:03:59 +0100

    hi

--- Dealerportal/src/main/java/com/qa/TestRunner/Vehicle_iden_runner.java
+++ Dealerportal/src/main/java/com/qa/TestRunner/Vehicle_iden_runner.java
@@ -11,6 +11,7 @@ import cucumber.api.junit.Cucumber;
 		glue={"vehicle_stepdefinition"}, //the path of the step definition files
 		
 		//format= {"pretty","html:test-outout"}, //to generate different types of reporting
+		
 		monochrome = true, //display the console output in a proper readable format
 		strict = false, //it will check if any step is not defined in step definition file
 		dryRun = false//to check the mapping is proper between feature file and step def file
--- Dealerportal/src/main/java/com/qa/pages/Dealerportal.java
+++ Dealerportal/src/main/java/com/qa/pages/Dealerportal.java
@@ -1,5 +1,6 @@
 package com.qa.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
@@ -8,24 +9,31 @@ import com.qa.util.Base;
 
 public class Dealerportal extends Base{
 
+	
+	static WebElement vehiclereg=driver.findElement(By.id("vehicleReg"));
+	static WebElement FindVehicle=driver.findElement(By.name("btnfind"));
+	
+	static WebElement homepagelogo=driver.findElement(By.xpath("//*[@id=\\\"header\\\"]/a/img"));
+	
+	
 	//page Factory
 	
+//	
+//	@FindBy(id="vehicleReg")
+//	WebElement vehiclereg;
+//	
+//	@FindBy(name="btnfind")
+//	WebElement FindVehicle;
+//	
+//	@FindBy(xpath="//*[@id=\"header\"]/a/img")
+//	WebElement homepagelogo;
 	
-	@FindBy(id="vehicleReg")
-	WebElement vehiclereg;
-	
-	@FindBy(name="btnfind")
-	WebElement FindVehicle;
-	
-	@FindBy(xpath="//*[@id=\"header\"]/a/img")
-	WebElement homepagelogo;
 	
 	
-	
-	public Dealerportal() {
-		
-		PageFactory.initElements(driver,this);
-	}
+//	public Dealerportal() {
+//		
+//		PageFactory.initElements(driver,this);
+//	}
 	
 	
 	
